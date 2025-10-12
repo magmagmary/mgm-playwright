7ac8fe4a5088a53f2abd26fafebf35caf9a1a1b3
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-10-12 16:15:45 +0200

    test: add extra test cases

--- CoolEvents/tests/Form.spec.ts
+++ CoolEvents/tests/Form.spec.ts
@@ -66,3 +66,124 @@ test('Form is not submitted if the user does not enter the minimal required fiel
   await expect(formSubmitted).toBe(false);
 
 });
+
+test('Form should not be submitted if the user does not confirm the dialog' ,async({page})=>{
+  let formSubmitted = false;
+
+  page.on('dialog', dialog => {
+      
+    if(dialog.message().includes('Do you really want to submit the form?')){
+      dialog.dismiss();
+      formSubmitted = false;
+    }
+
+    else{
+      dialog.accept();
+      formSubmitted = true;
+    }
+    });
+
+ await page.goto('http://localhost:5200/');
+
+ await page.getByTestId('accept-cookies').click();
+
+ await page.getByRole('link', { name: 'Go to Feedback Form' }).click();
+
+ await page.getByRole('textbox', { name: 'Name (required):' }).fill('Maryam');
+ await page.getByRole('textbox', { name: 'Email (required):' }).fill('magmag@gm.com');
+ await page.getByRole('textbox', { name: 'Comment (required):' }).fill('test');
+ await page.getByRole('checkbox', { name: 'I agree to the site\'s Terms' }).check();
+
+ await page.getByRole('button', { name: 'Submit' }).click();
+
+ await expect(formSubmitted).toBe(false);
+});
+
+test('Form is completed >> clear button should clear the form' ,async({page})=>{
+  page.on('dialog', dialog => {
+    dialog.accept();
+  });
+
+  await page.goto('http://localhost:5200/');
+
+  await page.getByTestId('accept-cookies').click();
+
+  await page.getByRole('link', { name: 'Go to Feedback Form' }).click();
+
+  const name = page.getByRole('textbox', { name: 'Name (required):' });
+
+  await name.fill('Maryam');
+
+  await page.getByRole('button', { name: 'Save Progress' }).click();
+
+  page.reload();
+
+  expect(name).toHaveValue('Maryam');
+
+  await page.getByRole('button', { name: 'Clear Progress' }).click();
+
+  await expect(name).toHaveValue('');
+});
+
+
+test('Form is completed >> Clear button should clear the memory' ,async({page})=>{
+  let formSubmitted = false;
+
+  page.on('dialog', dialog => {
+    dialog.dismiss();
+    formSubmitted = false;
+  });
+
+  await page.goto('http://localhost:5200/');
+
+  await page.getByTestId('accept-cookies').click();
+
+  await page.getByRole('link', { name: 'Go to Feedback Form' }).click();
+
+  const name = page.getByRole('textbox', { name: 'Name (required):' });
+
+  await name.fill('Maryam');
+
+  await page.getByRole('button', { name: 'Save Progress' }).click();
+
+  page.reload();
+
+  const storage = localStorage.getItem('name');
+
+  expect(storage).toBe(null);
+
+  await expect(name).toHaveValue('');
+});
+
+test('Form is completed >> Clear button should not clear the memory if the user does not confirm the dialog' ,async({page})=>{
+  page.on('dialog', dialog => {
+   if(dialog.message().includes('Are you sure you want to clear the form progress? This action cannot be undone.')){
+    dialog.dismiss();
+   }
+
+   else{
+    dialog.accept();
+   }
+  });
+
+  await page.goto('http://localhost:5200/');
+
+  await page.getByTestId('accept-cookies').click();
+
+  await page.getByRole('link', { name: 'Go to Feedback Form' }).click();
+
+  const name = page.getByRole('textbox', { name: 'Name (required):' });
+
+  await name.fill('Maryam');
+
+  await page.getByRole('button', { name: 'Save Progress' }).click();
+
+  page.reload();
+
+  await page.getByRole('button', { name: 'Clear Progress' }).click();
+
+  await expect(name).toHaveValue('Maryam');
+});
+
+
+
